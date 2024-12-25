d28dd9ffce479b2dafac650ba25b5063d46b4107
perilka <lisetskaya.evgeniya@yandex.ru>
2024-12-25 23:48:12 +0300

    the issue is solved

---
+++ rpn_issue.py
@@ -0,0 +1,63 @@
+class Stack:
+    def __init__(self): self.items = []
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if self.is_empty():
+            raise IndexError("Стек пуст")
+        return self.items.pop()
+
+    def peek(self):
+        if self.is_empty():
+            raise IndexError("Стек пуст")
+        return self.items[-1]
+
+    def size(self):
+        return len(self.items)
+
+    def clean(self):
+        while not self.is_empty():
+            self.pop()
+
+
+
+def calculate(token: str, a: int|float, b: int|float) -> int|float:
+    if token == '+':
+        return a + b
+    elif token == '-':
+        return b - a
+    elif token == '*':
+        return a * b
+    else:
+        return b / a
+
+
+def is_number(i):
+    try:
+        float(i)
+    except ValueError:
+        return False
+    else:
+        return True
+
+
+def rpn(string: str) -> int|float:
+    stack = Stack()
+    tokens = [x for x in string]
+    for token in tokens:
+        if is_number(token):
+            stack.push(float(token))
+        elif token in '+-*/':
+            a = stack.pop()
+            b = stack.pop()
+            stack.push(calculate(token, a, b))
+    return stack.peek()
+
+
+print(rpn('342*+'))
+print(rpn('72/'))
