9d01045ce172011a6923950aec778aec99f0d07a
perilka <lisetskaya.evgeniya@yandex.ru>
2025-01-17 02:19:01 +0300

    the issue is resolved

---
+++ DFS_issue.py
@@ -0,0 +1,104 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+
+        self.outbound = []
+        self.inbound = []
+
+    def point_to(self, other):
+        self.outbound.append(other)
+        other.inbound.append(self)
+
+    def __str__(self):
+        return f'Node({self.value})'
+
+
+class Graph:
+    def __init__(self, root: Node):
+        self._root = root
+
+        self.to_visit_list = [self._root]
+
+    def to_visit(self, node: Node) -> list[Node]:
+        neighbors = node.outbound
+        for neighbor in neighbors:
+            if neighbor not in self.to_visit_list:
+                self.to_visit_list.append(neighbor)
+                self.to_visit(neighbor)
+        return self.to_visit_list
+
+    def dfs(self):
+        for node in self.to_visit(self._root):
+            print(node, end=' ')
+
+
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+a.point_to(b)
+b.point_to(c)
+c.point_to(d)
+d.point_to(a)
+b.point_to(d)
+
+g = Graph(a)
+
+print(g.dfs())
+
+
+a = Node('a')
+b = Node('b')
+c = Node('c')
+a.point_to(b)
+b.point_to(c)
+
+g = Graph(a)
+
+print(g.dfs())
+
+
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+e = Node('e')
+f = Node('f')
+a.point_to(b)
+b.point_to(c)
+c.point_to(d)
+d.point_to(a)
+b.point_to(d)
+b.point_to(f)
+c.point_to(e)
+
+g = Graph(a)
+
+print(g.dfs())
+
+
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+e = Node('e')
+f = Node('f')
+g = Node('g')
+h = Node('h')
+i = Node('i')
+k = Node('k')
+a.point_to(b)
+b.point_to(c)
+c.point_to(d)
+d.point_to(a)
+b.point_to(d)
+a.point_to(e)
+e.point_to(f)
+e.point_to(g)
+f.point_to(i)
+f.point_to(h)
+g.point_to(k)
+
+g = Graph(a)
+
+print(g.dfs())
