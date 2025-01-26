a1b43d52bece538f4efdda87211cb1d0a472380c
perilka <lisetskaya.evgeniya@yandex.ru>
2025-01-26 23:55:41 +0300

    the issue is resolved

--- def_bubble_sort.py
+++ def_bubble_sort.py
@@ -1,13 +1,15 @@
 def bubble_sort(arr):
     changes = []
     while True:
-        for i in range(len(arr)-1):
+        j = 1
+        for i in range(len(arr)-j):
             if arr[i] > arr [i+1]:
                 a = arr[i]
                 b = arr[i+1]
                 arr[i+1] = a
                 arr[i] = b
                 changes.append(1)
+        j += 1 # в сл раз проверяем на один элемент меньше (с конца, само собой)
         changes.append(0)
         if changes[-1] == 0 and changes[-2] == 0:
             return arr
